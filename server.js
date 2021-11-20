f50e88e2e94bc55ef18209ace5743e84e08ed926
mayur.shedage <mayur.shedage@inscripts.in>
2021-11-20 21:22:57 +0530

    updated app & server

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
+var http = require('http');
 
-app.get('/', (req, res) => {
-    res.send('It Works!')
-})
+var port = process.env.PORT || 8081;
+app.set(port);
 
-app.listen(8081);
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+
+        default:
+            break;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    console.log('Listening on ' + bind);
+}
