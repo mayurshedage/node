b46292b888f5f89f39c70461be7adea16ec3fd15
mayur.shedage <mayur.shedage@inscripts.in>
2021-11-20 20:06:39 +0530

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const fs = require('fs');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/read', async (req, res) => {
+    const file = fs.readFileSync('read.json');
+
+    console.log(file);
+
+    res.status(200).json(file)
+});
+
+module.exports = app;
